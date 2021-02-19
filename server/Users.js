0e6e9fb6ea221dc7cd58e9cdef55c1b3f1ea29d8
skosana721 <wonganiskosana@gmail.com>
2021-02-19 13:02:05 +0200

    created an array of object with name , location and id

---
+++ server/Users.js
@@ -0,0 +1,7 @@
+const users = [
+  { id: 1, name: "James", location: "Diepsloot" },
+  { id: 2, name: "Francis", location: "Randburg" },
+  { id: 3, name: "Junior", location: "Sandton" },
+  { id: 4, name: "Arthur", location: "Fourways" },
+];
+module.exports = users;
