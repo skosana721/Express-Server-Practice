a22d59ce7a9563897e2fe29e5ce4b0dce252f2a4
skosana721 <wonganiskosana@gmail.com>
2021-02-19 10:27:38 +0200

    initialled an express server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const app = express;
+const app = express();
 
 const PORT = process.env.PORT || 7000;
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello World</h1>`);
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
