4f46333b3cbfdced1ccf84f49facef7a0824b88d
skosana721 <wonganiskosana@gmail.com>
2021-02-19 12:10:11 +0200

    created a rest api

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+const users = [
+  { id: 1, name: "James", location: "Diepsloot" },
+  { id: 2, name: "Francis", location: "Randburg" },
+  { id: 3, name: "Junior", location: "Sandton" },
+  { id: 4, name: "Arthur", location: "Fourways" },
+];
+
+//rest api
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+// setting static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 7000;
-
-app.get("/", (req, res) => {
-  res.send(`<h1>Hello World</h1>`);
-});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
