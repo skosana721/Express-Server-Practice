7cc712fe63f04c992f3f374791222018f40fd834
skosana721 <wonganiskosana@gmail.com>
2021-03-11 13:41:46 +0200

    add public folder whic an image, style.css file and index.html file and edited the expressServer.js file to render static file

--- express101/expressServer.js
+++ express101/expressServer.js
@@ -1,31 +1,13 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
 const PORT = 5000;
-// app.all("*", (req, res) => {
-//   res.send(`Hello , this is an express server`);
-// });
-
-// express routing
-// The browser default is a get request
-//CRUD app correspondence
-
-// get - READ
-// post - CREATE
-// delete - DELETE
-// put - UPDATE
-
-app.get("/", (req, res) => {
-  res.send(`<h1>This is the GET page!</h1>`);
+app.use(express.static("public"));
+app.all("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
-app.post("/", (req, res) => {
-  res.send(`<h1>This is the POST page!</h1>`);
-});
-app.delete("/", (req, res) => {
-  res.send(`<h1>This is the DELETE page!</h1>`);
-});
-app.put("/", (req, res) => {
-  res.send(`<h1>This is the PUT page!</h1>`);
+app.all("*", (req, res) => {
+  res.send(`<h1>Sorry page was not loaded</h1>`);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
