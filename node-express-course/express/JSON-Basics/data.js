b898bbeb75f79e7dff9a588f9f599c51b48b8468
skosana721 <wonganiskosana@gmail.com>
2021-04-12 15:48:05 +0200

    added ids to every objects

--- node-express-course/express/JSON-Basics/data.js
+++ node-express-course/express/JSON-Basics/data.js
@@ -1,8 +1,8 @@
 const user = [
-  { name: "James", age: 52 },
-  { name: "Peter", age: 22 },
-  { name: "John", age: 38 },
-  { name: "Frank", age: 19 },
-  { name: "Steve", age: 45 },
+  { id: 1, name: "James", age: 52 },
+  { id: 2, name: "Peter", age: 22 },
+  { id: 3, name: "John", age: 38 },
+  { id: 4, name: "Frank", age: 19 },
+  { id: 5, name: "Steve", age: 45 },
 ];
 module.exports = user;
